0b2794ad6e2f61ec81874af3d7a20d9cda4bf48e
Edward Stark <stark@MacBook-Pro.local>
2020-09-21 15:36:09 -0400

    add initial routes

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req,res)=>{
     res.render('landing');
+}); 
+
+app.get('/campgrounds', (req,res)=>{
+    let campgrounds = [
+        {name: 'Camp David', image: 'https://images.pexels.com/photos/2398220/pexels-photo-2398220.jpeg?auto=compress&cs=tinysrgb&h=350'},
+        {name: 'Camp Jarome', image: 'https://images.pexels.com/photos/1309584/pexels-photo-1309584.jpeg?auto=compress&cs=tinysrgb&h=350'},
+        {name: 'Camp John', image: 'https://pixabay.com/get/55e8dc404f5aab14f1dc84609620367d1c3ed9e04e50774975287ad1974bcc_340.jpg'}
+    ]
+
+    res.render('campgrounds', {campgrounds: campgrounds});
 });
 
 
